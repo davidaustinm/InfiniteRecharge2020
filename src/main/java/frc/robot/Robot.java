0d0b97761f96e0b870304673741c779802d7293a
Elijah Hagberg <e3ghe3gh@gmail.com>
2020-01-16 18:06:38 -0500

    Remove the extras

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,10 +7,9 @@
 
 package frc.robot;
 
+
 import edu.wpi.first.wpilibj.TimedRobot;
-
 import edu.wpi.first.wpilibj2.command.Command;
-
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.commands.*;
 import frc.robot.subsystems.*;
